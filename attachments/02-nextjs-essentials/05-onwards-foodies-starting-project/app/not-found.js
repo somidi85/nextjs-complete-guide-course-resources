34e6486e354cd9303e4a6a551278dd5944258315
somidi85 <somidi@eliya.ch>
2023-12-18 23:04:57 +0100

    Up to dynamic rendering

---
+++ attachments/02-nextjs-essentials/05-onwards-foodies-starting-project/app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>Page not found</h1>
+      <p>Unfortunately, we could not find the requested page.</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
